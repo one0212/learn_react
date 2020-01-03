0cb412154244cf0f111ff55df57afcd7fcaf5046
one0212 <feliciajiang12@gmail.com>
2020-01-03 16:25:32 +0800

    建立reducer

--- src/redux/todoList/index.js
+++ src/redux/todoList/index.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import { Input, Button, List } from "antd";
 
+import store from "./store";
 import "antd/dist/antd.css";
 
-const data = ["早上10點上班, 搭捷運到松山", "到公司前先買早餐", "下午4點下班"];
 
 class ReduxTodoList extends Component {
+  constructor(props) {
+    super(props)
+    // console.log(store.getState())
+    this.state = store.getState()
+  }
   render() {
     return (
       <div style={{ margin: "10px" }}>
         <div>
           <Input
-            placeholder="write something"
+            placeholder={this.state.inputValue}
             style={{ width: "250px", marginRight: "10px" }}
           />
           <Button type="primary">新增</Button>
@@ -19,7 +24,7 @@ class ReduxTodoList extends Component {
         <div style={{ margin: "10px", width: "300px" }}>
           <List
             bordered
-            dataSource={data}
+            dataSource={this.state.list}
             renderItem={item => <List.Item>{item}</List.Item>}
           />
         </div>
---
+++ src/redux/todoList/store/reducer.js
@@ -0,0 +1,9 @@
+const defaultState = {
+  inputValue: "Write Something",
+  list: [
+      "早上10點上班, 搭捷運到松山",
+      "到公司前先買早餐",
+      "下午4點下班"]
+};
+export default (state = defaultState, action) => state;
+
