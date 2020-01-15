a7056ea6b683952fcc57541522bb2386d1c18680
one0212 <feliciajiang12@gmail.com>
2020-01-15 12:02:48 +0800

    add combineReducers

--- src/redux/counter/index.js
+++ src/redux/counter/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { createStore } from 'redux';
-
+import { createStore, combineReducers } from 'redux';
+// 使用多個reducer要用到 combineReducers
 
 const counterReducer = function (state = { count: 1 }, action) {
   console.log(action)
@@ -19,16 +19,38 @@ const counterReducer = function (state = { count: 1 }, action) {
 
 }
 
+const postReducer = function(state = { list: [ {title: 'Hello'} ] }, action) {
+  switch(action.type) {
+    case 'LOAD_POSTS':
+      return {
+        ...state, list: action.payload
+      }
+    default: 
+      return state
+  }
+}
+
+// 通過combineReducer
+const rootReducers = combineReducers({
+  counter: counterReducer,
+  post: postReducer
+})
+
 // 建立一個store
 const store = createStore(
-  counterReducer,
+  // counterReducer, // 由rootReducer取代
+  rootReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 
 console.log(store)
 console.log(store.getState())
 
- // 用於修改counter的值
+//  如果要改變一個reducer的值 需要使用dispatch派發一個action
+//  action需要兩個參數
+//  type  通過type區分是對state做什麼操作
+//  payload  傳遞的數據
+// 用於修改counter的值
  store.dispatch({
   type: "COUNT_ADD",
   payload: {},  
@@ -44,6 +66,10 @@ store.dispatch({
 
 console.log(store)
 console.log(store.getState())
+
+const getPostRequest = () => {
+  return get ('https://jsonplaceholder.typicode.com/posts')
+}
 
 
 class Counter extends Component {
