20aa51734751d3e45e87c5c197bf065f837798b4
one0212 <feliciajiang12@gmail.com>
2020-01-15 12:23:36 +0800

    add middleware - redux-thunk

--- src/redux/counter/index.js
+++ src/redux/counter/index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
-import { createStore, combineReducers } from 'redux';
 // 使用多個reducer要用到 combineReducers
+// 要使用middleware需加入compose, applyMiddleware
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+
+import { get } from 'axios';
+
+// redux中要使用異步操作 就要使用middleware
+// redux-thunk 可以發送一個異步請求
+import thunk from 'redux-thunk';
 
 const counterReducer = function (state = { count: 1 }, action) {
   console.log(action)
@@ -40,7 +47,10 @@ const rootReducers = combineReducers({
 const store = createStore(
   // counterReducer, // 由rootReducer取代
   rootReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(...[thunk]), // 需要使用的中間健陣列
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
   )
 
 console.log(store)
@@ -68,9 +78,18 @@ console.log(store)
 console.log(store.getState())
 
 const getPostRequest = () => {
-  return get ('https://jsonplaceholder.typicode.com/posts')
+  return get('https://jsonplaceholder.typicode.com/posts')
 }
 
+// 使用thunk後 所有action中就會有dispatch參數
+store.dispatch(async function(dispatch) {
+  const res = await getPostRequest()
+  console.log(res.data)
+  dispatch({
+    type: 'LOAD_POSTS',
+    payload: res.data
+  })
+})
 
 class Counter extends Component {
 
